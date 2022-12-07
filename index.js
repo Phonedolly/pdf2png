132c2dea13e106550d745cd9f762755312e55f81
Phonedolly <Phonedolly@users.noreply.github.com>
2022-12-07 15:19:45 +0900

    refactored to use pdf-img-convert

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const pdf2png = require('pdf2png-mp2');
+/**
+ * index.js
+ */
+const fs = require("fs");
+const path = require("path");
+const pdf2img = require('pdf-img-convert');
 
-pdf2png.convert(path.join(__dirname, process.argv[2]), { quality: 300 }, function (resp) {
-    if (!resp.success) {
-        console.log("Something went wrong: " + resp.error);
+if (process.argv[2] === undefined) {
+    console.log("specify target file");
+    return;
+}
 
-        return;
-    }
+if (!fs.existsSync(path.join(path.dirname(process.argv[2]), `${path.basename(process.argv[2])}_out`))) {
+    fs.mkdirSync(path.join(path.dirname(process.argv[2]), `${path.basename(process.argv[2])}_out`));
+}
 
-    console.log("Yayy the pdf got converted, now I'm gonna save it!");
-    if (!fs.existsSync(path.join(__dirname, `/${process.argv[2]}_out`))) {
-        fs.mkdirSync(path.join(__dirname, `/${process.argv[2]}_out`));
-    }
-    for (i = 1; i < resp.data.length; i++) {
-        fs.writeFile(path.join(__dirname, `${process.argv[2]}_out`, process.argv[2] + `.${i}.png`), resp.data[i], function (err) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log("file saved!");
-            }
-        });
-    }
-});
+pdf2img.convert(process.argv[2], { height: 2560 })
+    .then((convertedResult) => {
+        for (i = 0; i < convertedResult.length; i++) {
+            fs.writeFile(path.join(path.dirname(process.argv[2]), `${path.basename(process.argv[2])}_out`, path.basename(process.argv[2] + "." + i + ".png")), convertedResult[i], (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                else {
+                    console.log(path.join(path.dirname(process.argv[2]), `${path.basename(process.argv[2])}_out`, path.basename(process.argv[2] + "." + i + ".png")) + " is saved!");
+                }
+            })
+        }
+    })
